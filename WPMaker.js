b55df8886eb9da0b9f1d453e67b2dd638ae8e98d
Alexandr Vorobyev <alex_vor02@mail.ru>
2020-04-24 16:51:05 +0300

    Update WPMaker.js

--- WPMaker.js
+++ WPMaker.js
@@ -1,18 +1,46 @@
-let canvas = document.getElementById('cnv')
+'use strict'
+let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+let backgroundPicture = new Image()
+backgroundPicture.src = canvas.toDataURL()
+$('.te-form').hide()
 
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+let clearForm = () => {
+  $('#textWP').val('')
+  $('#widthTextPos').val(50)
+  $('#heightTextPos').val(50)
+}
+
 
 let draw = () => {
+  ctx.drawImage(backgroundPicture, 0, 0, canvas.width, canvas.height)
 
-  ctx.fillStyle = "#000";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  ctx.fillStyle = "#FFF";
-  ctx.font = "72pt Impact";
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
+  ctx.fillStyle = '#FFF'
+  ctx.font = '72pt Impact'
+  ctx.textAlign = 'center'
+  ctx.textBaseline = 'middle'
   ctx.fillText($('#textWP').val(), $('#widthTextPos').val() / 100 * 1080,
-              (100 - $('#heightTextPos').val()) / 100 * 1920);
+    (100 - $('#heightTextPos').val()) / 100 * 1920)
 }
+
+let newText = () => {
+  $('.te-button').hide()
+  $('.te-form').show()
+}
+
+let saveText = () => {
+  $('.te-button').show()
+  $('.te-form').hide()
+  backgroundPicture.src = canvas.toDataURL()
+  clearForm()
+}
+
+let deleteText = () => {
+  $('.te-button').show()
+  $('.te-form').hide()
+  clearForm()
+}
+
 setInterval(draw, 10)
